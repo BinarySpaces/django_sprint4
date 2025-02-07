ef3dc201337cff6493f2c4f2ade08d39e20ec06e
Andrey Starikov <flashbum2003@yandex.ru>
2025-02-07 18:18:59 +0300

    some updates

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-# from django.utils import timezone
+from django.utils import timezone
 from django import forms
 
 from . models import Comment, Post
@@ -12,23 +12,33 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        exclude = ['author',]
+        exclude = ('author',)
         widgets = {
             'pub_date': forms.DateInput(attrs={
                 'type': 'date', 'placeholder': 'DD.MM.YYYY'
             })
         }
 
+    def clean(self):
+        cleaned_data = super().clean()
+        pub_date = cleaned_data.get('pub_date')
+
+        if pub_date and pub_date > timezone.now():
+            cleaned_data['is_published'] = False
+        else:
+            cleaned_data['is_published'] = cleaned_data.get('is_published')
+        return cleaned_data
+
 
 class UserProfileForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email',]
+        fields = ('username', 'first_name', 'last_name', 'email',)
 
 
 class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ['text',]
+        fields = ('text',)
